88e0c10bf42993d077a900b143daa81aef82e2f3
mhann-ncsu <mhann@ncsu.edu>
2024-06-27 13:20:05 -0400

    Update fvgThread.py
    Implemented file export to .csv and .xlsx based on process parameters entered in dialog window

--- GUI/fvgThread.py
+++ GUI/fvgThread.py
@@ -1,26 +1,47 @@
+import numpy as np
 import os
 import pandas as pd
-from PyQt5.QtCore import pyqtSignal, QThread
+from PyQt5.QtCore import *
+import xlsxwriter
 
 class FVGThread(QThread):
     complete_signal = pyqtSignal()
 
-    def __init__(self, filenames, outputFilename):
+    def __init__(self, outputFilename, params):
         QThread.__init__(self)
-        self.filenames = filenames
-        self.outputFilename = outputFilename
         self.running = True
 
+        self.outputFilename = outputFilename
+        self.params = params
+
     def __del__(self):
-        self.wait()
+        try:
+            self.wait()
+        except RuntimeError:
+            return
 
     def run(self):
         self.running = True
 
-        if self.running:
-            self.save_features()
-            self.complete_signal.emit()
+        paramsList = []
+        valuesList = []
+        for i in self.params.keys():
+            paramsList.append(i)
+            valuesList.append(self.params[i])
+        d = {'Parameters': paramsList, 'Values': valuesList}
+        df = pd.DataFrame(data=d)
 
+        if self.running:
+            try:
+                if self.outputFilename.split('.')[-1] == 'csv':
+                    # df.to_csv(self.outputFilename)
+                    print(rf'File: {self.outputFilename}'.replace('\\', '/'))
+                if self.outputFilename.split('.')[-1] == 'xlsx':
+                    print(rf'File: {self.outputFilename}'.replace('\\', '/'))
+                    df.to_excel(self.outputFilename, sheet_name='Sheet1', index=False, engine='xlsxwriter')
+            except OSError:
+                print("Didn't export, file is open in another program.")
+            self.complete_signal.emit()
 
     def stop(self):
         self.running = False
